4cc9b65c02b86218cc38a1832dceebb2c6dec78b
birkhoffkiki <birkhoffkiki@gmail.com>
2025-03-09 00:11:09 +0800

    fix virchow's output

--- models/virchow.py
+++ models/virchow.py
@@ -1,31 +1,47 @@
 # https://huggingface.co/paige-ai/Virchow
 import timm
-from torchvision import transforms
 import torch
 from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
 from timm.layers import SwiGLUPacked
-from PIL import Image
     
     
 def get_virchow_trans():
-    # transform = transforms.Compose(
-    #     [
-    #         transforms.Resize((224, 224)),
-    #         transforms.ToTensor(),
-    #         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-    #     ]
-    # )
     model = timm.create_model("hf-hub:paige-ai/Virchow", pretrained=True, mlp_layer=SwiGLUPacked, act_layer=torch.nn.SiLU)
     transforms = create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
+    del model
     return transforms
 
+
 def get_virchow_model(device):
-    model = timm.create_model("hf-hub:paige-ai/Virchow", pretrained=True, mlp_layer=SwiGLUPacked, act_layer=torch.nn.SiLU)
-    # model = timm.create_model(
-    #     "vit_huge_patch14_224_in21k", img_size=224, patch_size=14, init_values=1e-5, num_classes=0, dynamic_img_size=True, mlp_layer=SwiGLUPacked, act_layer=torch.nn.SiLU
-    # )
-    # model.load_state_dict(torch.load('models/ckpts/.bin', map_location="cpu"), strict=True)
-    # model.eval()
-    model = model.eval()
-    return model.to(device)
+    try:
+        model = timm.create_model("hf-hub:paige-ai/Virchow", pretrained=True, 
+                                mlp_layer=SwiGLUPacked, act_layer=torch.nn.SiLU).to(device)
+    except:
+        print('Failed to load pretrained model from huggingface, loading from local')
+        model = timm.create_model("hf-hub:paige-ai/Virchow", pretrained=False, 
+                                mlp_layer=SwiGLUPacked, act_layer=torch.nn.SiLU).to(device)
+        msg = model.load_state_dict(torch.load('models/ckpts/virchow.pth'))
+        print(msg)
+
+    model.eval()
+    def func(image):
+        # get the features
+        with torch.inference_mode():
+            output = model(image)
+
+        class_token = output[:, 0]    # size: 1 x 1280
+        patch_tokens = output[:, 1:]  # size: 1 x 256 x 1280, tokens 1-4 are register tokens so we ignore those
+        # concatenate class token and average pool of patch tokens
+        embedding = torch.cat([class_token, patch_tokens.mean(1)], dim=-1)  # size: 1 x 2560
+        return embedding # float32
+    
+    return func
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = get_virchow_model(device)
+    img = torch.randn(1, 3, 224, 224).to(device)
+    print(model(img).shape)
+    print('Virchow model loaded successfully')
+    
