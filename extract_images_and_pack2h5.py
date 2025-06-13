41d3071a854b303c95d520e92a5928e83d3d4303
birkhoffkiki <birkhoffkiki@gmail.com>
2025-06-13 23:45:53 +0800

    add support for extracting feature of packed h5 images

--- extract_images_and_pack2h5.py
+++ extract_images_and_pack2h5.py
@@ -1,30 +1,26 @@
-import openslide
 import os
+import io
 import h5py
 import numpy as np
 from multiprocessing.pool import Pool
 import glob
 import argparse
-from wsi_core.WholeSlideImage import ImgReader
-import openslide
+from wsi_core.Aslide.aslide import Slide
 
 
 def get_wsi_handle(wsi_path):
     if not os.path.exists(wsi_path):
         raise FileNotFoundError(f'{wsi_path} is not found')
-    postfix = wsi_path.split('.')[-1]
-    if postfix.lower() in ['svs', 'mrxs', 'tiff', 'tif']:
-        handle = openslide.OpenSlide(wsi_path)
-    else:
-        handle = ImgReader(wsi_path)
-
+    handle = Slide(wsi_path)
     return handle
 
 
 def read_images(arg):
-    CHUNK_SIZE=64
-    h5_path, save_path, wsi_path, size, level = arg
+    h5_path, save_path, wsi_path = arg
     if wsi_path is None:
+        return
+    if os.path.exists(save_path):
+        print(f'{save_path} already exists, skipping...')
         return
 
     print('Processing:', h5_path, wsi_path, flush=True)
@@ -32,103 +28,78 @@ def read_images(arg):
         h5 = h5py.File(h5_path)
     except:
         print(f'{h5_path} is not readable....')
-        return    
-    _num = len(h5['coords'])
-
-    coors = h5['coords']
-    wsi_handle = get_wsi_handle(wsi_path)
-    # create a h5 file to pack all PIL images
+        return
     
-    # for x, y in coors:
-    #     p = os.path.join(save_root, '{}_{}_{}_{}.jpg'.format(x, y, size[0], size[1]))
-
-    #     img = wsi_handle.read_region((x, y), level, size).convert('RGB')
-    #     img.save(p)
-
-    with h5py.File(save_path, 'w') as h5_file:
-        # create dataset
-        dataset = h5_file.create_dataset(
+    _num = len(h5['coords'])
+    coors = h5['coords']
+    level = h5['coords'].attrs['patch_level']
+    size = h5['coords'].attrs['patch_size']
+    
+    wsi_handle = get_wsi_handle(wsi_path)
+    with h5py.File(save_path+'.temp', 'w') as h5_file:
+        # 创建变长数据集存储JPEG字节流
+        patches_dataset = h5_file.create_dataset(
             'patches',
-            shape=(_num, size, size, 3),
-            maxshape=(None, size, size, 3), 
-            chunks=(CHUNK_SIZE, size, size, 3), 
-            dtype=np.uint8,
-            compression='gzip',
-            compression_opts=4,
-        )
-        
-        # create file names dataset
-        names_dataset = h5_file.create_dataset(
-            'filenames',
             shape=(_num,),
             maxshape=(None,),
-            dtype=h5py.string_dtype(encoding='utf-8')
+            dtype=h5py.vlen_dtype(np.uint8),  # 变长字节数组
+            compression='gzip',
+            compression_opts=6
         )
         
-        # 逐图像处理
+        # 逐图像处理并存储为JPEG
         for i, (x, y) in enumerate(coors):
-            img_file = '{}_{}_{}_{}.jpg'.format(x, y, size[0], size[1])
-            img = wsi_handle.read_region((x, y), level, size).convert('RGB')
-        
-            img_array = np.array(img)
-            dataset[i] = img_array
-            names_dataset[i] = img_file
+            img = wsi_handle.read_region((x, y), level, (size, size)).convert('RGB')
             
+            # 将图像编码为JPEG字节流
+            with io.BytesIO() as buffer:
+                img.save(buffer, format='JPEG')
+                jpeg_bytes = buffer.getvalue()
+            
+            # 存储JPEG字节流
+            patches_dataset[i] = np.frombuffer(jpeg_bytes, dtype=np.uint8)
+    
+    os.rename(save_path+'.temp', save_path)
     print(f"{wsi_path} finished!")
 
-def get_wsi_path(wsi_root, h5_files, datatype, wsi_format):
+def get_wsi_path(wsi_root, h5_files, wsi_format):
     kv = {}
-    if datatype.lower() == 'tcga':
-        _files = os.listdir(wsi_root)
-        _file = [f for f in _files if '.txt' in f and 'output' not in f][0]
-        print('manifist file is:', _file)
-        with open(os.path.join(wsi_root, _file)) as f:
-            for l in f:
-                if 'id\tfilename' in l:
-                    continue
-                meta = l.split('\t')
-                # print(meta)
-                k = meta[1][:-4]
-                # print('parser:', k)
-                kv[k] = os.path.join(wsi_root, meta[0])
-    elif datatype.lower() == 'single_folder':
-        _files = os.listdir(wsi_root)
-        _files = [f for f in _files if f.split('.')[-1]==wsi_format]
-        for l in _files:
-            svs_id = l[:-len(wsi_format)-1]
-            kv[svs_id] = wsi_root
-    elif datatype.lower() == 'auto':
-        # auto search path
-        all_paths = glob.glob(os.path.join(wsi_root, '**'), recursive=True)
-        all_paths = [i for i in all_paths if f'.{wsi_format}' in i]
-        for h in h5_files:
-            prefix = os.path.splitext(h)[0]
-            wsi_file_name = '{}.{}'.format(prefix, wsi_format)
-            p = [i for i in all_paths if wsi_file_name == os.path.split(i)[-1]]
-            # print(wsi_file_name, p)
-            # print(all_paths)
-            if len(p) != 1:
-                print('failed to process:', p)
-                kv[prefix] = None
-                continue
-                # raise RuntimeError
-            p = os.path.split(p[0])[0]
-            kv[prefix] = p
 
-    else:
-        raise NotImplementedError(f'{datatype} is not implementated...')
+    # Convert wsi_format to list if it's not already
+    formats = [wsi_format] if isinstance(wsi_format, str) else wsi_format
+    # auto search path
+    all_paths = glob.glob(os.path.join(wsi_root, '**'), recursive=True)
+    # Check for any of the formats
+    all_paths = [i for i in all_paths if any(f'.{fmt}' in i for fmt in formats)]
+    
+    for h in h5_files:
+        prefix = os.path.splitext(h)[0]
+        # Try each format until we find a match
+        for fmt in formats:
+            wsi_file_name = f'{prefix}.{fmt}'
+            p = [i for i in all_paths if wsi_file_name == os.path.split(i)[-1]]
+            if len(p) == 1:
+                kv[prefix] = os.path.split(p[0])[0]
+                break
+        else:  # No break occurred, no match found
+            print('failed to process:', prefix)
+            kv[prefix] = None
 
     wsi_paths = []
     for h in h5_files:
-        # prefix = h[:-3]
         prefix = os.path.splitext(h)[0]
-        print(prefix)
         r = kv[prefix]
         if r is None:
             p = None
         else:
-            p = os.path.join(r, prefix+'.'+wsi_format)
-
+            # Find which format was actually matched
+            matched_format = None
+            for fmt in formats:
+                if os.path.exists(os.path.join(r, f'{prefix}.{fmt}')):
+                    matched_format = fmt
+                    break
+            p = os.path.join(r, f'{prefix}.{matched_format}') if matched_format else None
+        
         wsi_paths.append(p)
     
     return wsi_paths
@@ -136,26 +107,26 @@ def get_wsi_path(wsi_root, h5_files, datatype, wsi_format):
 
 def argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--datatype')
-    parser.add_argument('--wsi_format')
-    parser.add_argument('--level', type=int, default=0)
-    parser.add_argument('--size', type=int, default=512)
+    parser.add_argument('--wsi_format', type=str, default='svs', help="WSI file format(s) to search for. "
+         "For multiple formats, separate with semicolons (e.g., 'svs;tif;ndpi'). "
+         "The function will try each format in order until it finds a match.")
     parser.add_argument('--cpu_cores', type=int, default=48)
-    parser.add_argument('--h5_root')
-    parser.add_argument('--save_root')
-    parser.add_argument('--wsi_root')
+    parser.add_argument('--h5_root', help="Root directory containing the coors (.h5) files. "
+             "These files typically contain extracted patches or features from WSIs. "
+             "This is a required parameter.")
+    parser.add_argument('--save_root', help="Root directory where processed outputs will be saved. "
+             "The tool will create necessary subdirectories here. "
+             "This is a required parameter.")
+    parser.add_argument('--wsi_root', help="Root directory containing the whole slide image files. "
+             "The tool will search recursively in this directory for WSIs. "
+             "This is a required parameter.")
     return parser
 
 
 if __name__ == '__main__':
     parser = argparser().parse_args()
 
-    datatype = parser.datatype
     wsi_format = parser.wsi_format
-    level = parser.level
-    size = parser.size
-    size = (size, size)
-
     h5_root = parser.h5_root
     save_root = parser.save_root
     wsi_root = parser.wsi_root
@@ -163,9 +134,9 @@ if __name__ == '__main__':
     
     h5_files = os.listdir(h5_root)
     h5_paths = [os.path.join(h5_root, p) for p in h5_files]
-    wsi_paths = get_wsi_path(wsi_root, h5_files, datatype, wsi_format)
+    wsi_paths = get_wsi_path(wsi_root, h5_files, wsi_format)
     save_roots = [os.path.join(save_root, i) for i in h5_files]
-    args = [(h5, sr, wsi_path, size, level) for h5, wsi_path, sr in zip(h5_paths, wsi_paths, save_roots)]
+    args = [(h5, sr, wsi_path) for h5, wsi_path, sr in zip(h5_paths, wsi_paths, save_roots)]
 
     mp = Pool(parser.cpu_cores)
     mp.map(read_images, args)
